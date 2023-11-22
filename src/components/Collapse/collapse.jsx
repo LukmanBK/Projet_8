8260b9e6cac5f1254d76c4d5becafa284bf978f1
LukmanBK <lukmanbakari@gmail.com>
2023-11-22 18:08:37 +0100

    correctifs collapse

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
 import style from '../Collapse/Collapse.module.scss'
-import expand from '../../assets/arrowExpand.png'
+import contract from '../../assets/arrowContract.png'
 import { PropTypes } from 'prop-types'
 
 
@@ -22,7 +22,7 @@ export default function Collapse (props) {
         <div className={hidden ? style.collapseClose : style.collapseOpen}>
             <button onClick={handleDropdownClick}>
                 <h2>{props.title}</h2>
-                <img src={expand} alt='arrow' className={hidden ? style.collapseArrow__close : style.collapseArrow__open} />
+                <img src={contract} alt='arrow' className={hidden ? style.collapseArrow__close : style.collapseArrow__open} />
             </button>
             <div className={style.collapseContent} ref={containerHeight} style={{height: hidden ? `${contentHeight}` : '0px'}}>{props.detail}</div>
         </div>
